b2a99c8bd70afb6a594ba2d3cb70ce25412a186d
Junior Christofoli <34336694+juniorfdtech@users.noreply.github.com>
2019-07-24 15:47:44 -0300

    Update corrige.sh

--- corrige.sh
+++ corrige.sh
@@ -5,6 +5,6 @@ rm -rf /etc/stunnel/stunnel.pem
 wget https://raw.githubusercontent.com/juniorfdtech/sslmanager/master/stunnel.pem -O /etc/stunnel/stunnel.pem > /dev/null 2>&1
 chmod 777 /etc/stunnel/stunnel.pem
 service stunnel4 restart
-rm corrige-ssl.sh
+rm corrige.sh
 cat /dev/null > ~/.bash_history && history -c && clear
 exit
